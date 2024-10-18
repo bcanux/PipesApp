32e5a9bd33e2938bc3d4ab274eca12b72bc29218
Brandon Canux <bcanuxj@outlook.com>
2024-10-18 15:30:44 -0600

    Primeros cambios

---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem, PrimeIcons } from 'primeng/api';
+import { FloatLabel } from 'primeng/floatlabel';
+import { Routes, Router } from '@angular/router';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styles: ``
+})
+export class MenuComponent implements OnInit {
+
+  public menuItems: MenuItem[] | undefined;
+  constructor(private router:  Router){}
+
+  ngOnInit() {
+    this.menuItems = [
+     {
+      label: 'Pipes de Angular',
+      icon: 'pi pi-desktop',
+      items: [
+        {
+          label: 'Textos y Fechas',
+          icon: 'pi pi-align-left',
+          routerLink: 'basics',
+          // command:()=>{this.router.navigate(['/basics'])}
+
+        },
+        {
+          label: 'Números',
+          icon: 'pi pi-dollar',
+          routerLink: 'numbers'
+
+          // command:()=>{this.router.navigate(['/numbers'])}
+        },
+        {
+          label: 'No comunes',
+          icon: 'pi pi-globe',
+          routerLink: 'uncommon'
+          //command:()=>{this.router.navigate(['/uncommon'])}
+        }
+      ]
+
+     },
+     {
+      label: 'Pipes personalizados',
+      icon: 'pi pi-cog',
+      items: [
+        {
+          label: 'Otros elemento',
+          icon: 'pi pi-cog'
+        }
+      ]
+     }
+    ];
+  }
+}
